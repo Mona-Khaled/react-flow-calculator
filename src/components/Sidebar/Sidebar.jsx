22497bc3987191c675fd94fe8e3fde3d255da96c
Mona Khaled <mnkhaled1@gmail.com>
2024-05-12 01:52:13 +0300

    Collapsable Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,101 +4,104 @@ import { Sidebar } from "flowbite-react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import "./index.css";
 import menuItems from "./menu-items";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import MyContext from "../../Context";
+import { BsTextIndentRight, BsTextIndentLeft } from "react-icons/bs";
 
 const SidebarComponent = ({ tab, setTab, setDropPosition, dropPosition }) => {
   const { setNodeIdx, nodes } = useContext(MyContext);
-  const [{ left, top }, setPosition] = useState({ left: 0, top: 0 });
-  const [{ clientX, clientY }, setClient] = useState({
-    clientX: 0,
-    clientY: 0,
-  });
-
-  useEffect(() => {
-    console.log("🚀 ~ useEffect ~ dropPosition:", dropPosition);
-  }, [dropPosition]);
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const dragEnd = (e) => {
     e.preventDefault();
     setDropPosition({ x: e.clientX, y: e.clientY });
-
-    // console.log("🚀 ~ SidebarComponent ~ clientX:", e.clientX);
-    // console.log("🚀 ~ SidebarComponent ~ clientY:", e.clientY);
-    // setDropPosition({ x: e.clientX - 160, y: e.clientY - 160 });
     setTab(e.target.innerText);
   };
   const dragOver = (e) => {
     e.preventDefault();
   };
+  const toggleCollapse = () => setIsCollapsed(!isCollapsed);
+
   return (
-    <Sidebar aria-label="Default sidebar example" className="sidebar">
-      <Sidebar.Logo
-        className="logo"
-        href="#"
-        img="logo.svg"
-        imgAlt="Calculator Logo"
+    <>
+      <Sidebar
+        aria-label="Default sidebar example"
+        className={`sidebar ${isCollapsed ? "collapsed" : ""}`}
       >
-        <h3>DnD-Calculator</h3>
-      </Sidebar.Logo>
-
-      <Sidebar.Items
-        className="items"
-        onClick={(e) => {
-          setTab(e.target.innerText);
-          setNodeIdx(nodes.length + 1);
-        }}
-        onDragOver={dragOver}
-      >
-        <Sidebar.ItemGroup className="item-group" onDragOver={dragOver}>
-          {menuItems.map((item, index) => (
-            <Sidebar.Item
-              icon={item.icon}
-              className="item box"
-              key={index}
-              draggable="true"
-              // onDragStart={dragStart}
-              onDrag={(e) => {
-                console.log("🚀 ~ SidebarComponent ~ clientX:", e.clientX);
-                console.log("🚀 ~ SidebarComponent ~ clientY:", e.clientY);
-                // setDropPosition({ x: e.clientX, y: e.clientY });
+        {!isCollapsed ? (
+          <div
+            style={{
+              display: "flex",
+              height: "50px",
+              borderBottomColor: "#F3F4F6",
+            }}
+          >
+            <div
+              style={{
+                width: "300px",
+                backgroundColor: "#F3F4F6",
               }}
-              onDragOver={(e) => {
-                setClient({ clientX: e.clientX, clientY: e.clientY });
-              }}
-              // onDragEnd={dragEnd}
-
-              onDragStart={(e) => {
-                setClient({ clientX: e.clientX, clientY: e.clientY });
-              }}
-              onDragEnd={
-                dragEnd
-                //   (e) => {
-                //   const clientRect = e.currentTarget.getBoundingClientRect();
-                //   console.log("🚀 ~ SidebarComponent ~ clientX:", clientX);
-                //   console.log("🚀 ~ SidebarComponent ~ clientY:", clientY);
-                //   e.preventDefault();
-                //   setDropPosition({
-                //     x: e.clientX,
-                //     y: e.clientY,
-                //   });
-                //   setTab(e.target.innerText);
-
-                //   setPosition({
-                //     left: clientRect.left + e.clientX - clientX,
-                //     top: clientRect.top + e.clientY - clientY,
-                //   });
-                // }
-              }
             >
-              <div className="item-content">
-                {item.label}
-                <RxHamburgerMenu className="hamburger-icon" />
-              </div>
-            </Sidebar.Item>
-          ))}
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+              <img src="logo.svg" alt="logo" height="90%" />
+            </div>
+            <button
+              onClick={toggleCollapse}
+              style={{
+                border: "none",
+                backgroundColor: "#F3F4F6",
+                width: "10%",
+                cursor: "pointer",
+                fontSize: "24px",
+              }}
+            >
+              <BsTextIndentRight />
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={toggleCollapse}
+            style={{
+              border: "none",
+              backgroundColor: "#1E272E",
+              color: "#F3F4F6",
+              cursor: "pointer",
+              fontSize: "24px",
+            }}
+          >
+            <BsTextIndentLeft />
+          </button>
+        )}
+
+        <Sidebar.Items
+          className="items"
+          onClick={(e) => {
+            setTab(e.target.innerText);
+            setNodeIdx(nodes.length + 1);
+          }}
+          onDragOver={dragOver}
+        >
+          <Sidebar.ItemGroup className="item-group" onDragOver={dragOver}>
+            {menuItems.map((item, index) => (
+              <Sidebar.Item
+                icon={item.icon}
+                className="item box"
+                key={index}
+                draggable="true"
+                // onDragStart={dragStart}
+                onDrag={(e) => {
+                  // setDropPosition({ x: e.clientX, y: e.clientY });
+                }}
+                onDragEnd={dragEnd}
+              >
+                <div className="item-content">
+                  {item.label}
+                  <RxHamburgerMenu className="hamburger-icon" />
+                </div>
+              </Sidebar.Item>
+            ))}
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </>
   );
 };
 
